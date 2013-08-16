e99281e5090b9cbf36ec5d854a1c86b12931941f
Iurii Kozuliak <i.kozuliak@gmail.com>
2013-08-17 00:28:21 +0200

    Redesign :) basic geocoding and image searching

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -23,24 +23,94 @@
         _init:function () {
 
 
+            this._initMap();
 
-            google.maps.event.addDomListener(window, 'load', this._initMap);
-
-
+            this._initEvents();
         },
 
         _initMap: function(){
+
+            this.geocoder = new google.maps.Geocoder();
+
             var mapOptions = {
                 zoom: 8,
                 center: new google.maps.LatLng(-34.397, 150.644),
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
-            var map = new google.maps.Map(document.getElementById('map-container'),
+
+            this.map = new google.maps.Map(document.getElementById('map-container'),
                 mapOptions);
+        },
+
+        _initSearch : function(){
+
+            this.imageSearch = new google.search.ImageSearch();
+            this.imageSearch.setRestriction(
+                google.search.ImageSearch.RESTRICT_IMAGESIZE,
+                google.search.ImageSearch.IMAGESIZE_Medium);
+            this.imageSearch.setSearchCompleteCallback(this, searchComplete, null);
+
+            this.imageSearch.execute("Wroclaw");
+
+        },
+
+        _onSearchComplete : function(){
+            if (this.imageSearch.results && this.imageSearch.results.length > 0) {
+
+                var contentDiv = document.getElementById('search-results');
+                contentDiv.innerHTML = '';
+
+                var results = this.imageSearch.results;
+                for (var i = 0; i < results.length; i++) {
+                    var result = results[i];
+                    var imgContainer = document.createElement('div');
+                    var title = document.createElement('div');
+
+                    title.innerHTML = result.titleNoFormatting;
+                    var newImg = document.createElement('img');
+
+                    newImg.src=result.url;
+                    imgContainer.appendChild(title);
+                    imgContainer.appendChild(newImg);
+
+                    contentDiv.appendChild(imgContainer);
+                }
+            }
+        },
+
+
+        _geoCoder : function(){
+
+            var self = this;
+
+            var address = document.getElementById('geocoder').value;
+
+            this.geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    self.map.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        map: self.map,
+                        position: results[0].geometry.location,
+                        title : "test",
+                        animation: google.maps.Animation.DROP
+                    });
+
+                    console.log(marker)
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+
         },
 
         _initEvents:function () {
 
+            var self = this;
+
+            document.getElementById('geocoder').addEventListener('change', function(){
+                self._geoCoder();
+                console.log(document.getElementById('geocoder').value);
+            })
 
         }
     }
